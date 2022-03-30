69071df0d835fca32785626c0e9c0958c7d9a41b
AMarinaG <alberto.marina.gonzalo@gmail.com>
2022-03-30 11:58:44 +0200

    send extras to second activity

--- app/src/main/java/com/amarinag/demo1/MainActivity.kt
+++ app/src/main/java/com/amarinag/demo1/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -15,12 +16,29 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val btnNext: Button = findViewById(R.id.btn_next)
+        val tvTitle: TextView = findViewById(R.id.tv_title)
+        val etUsername: EditText = findViewById(R.id.et_name)
+
+        tvTitle.setOnClickListener {
+            etUsername.setText("Username secure")
+        }
+        etUsername.setOnLongClickListener(object : View.OnLongClickListener {
+            override fun onLongClick(p0: View?): Boolean {
+                etUsername.setText("Username secure")
+                return true
+            }
+
+        })
 
         btnNext.setOnClickListener(object : View.OnClickListener {
             override fun onClick(p0: View?) {
-                Log.d("AMG", "Buton pulsado")
-                Toast.makeText(this@MainActivity, "btn clicked", Toast.LENGTH_SHORT).show()
+                val text = etUsername.text.toString()
+                Log.d("AMG", "Buton pulsado $text")
+                Toast.makeText(this@MainActivity, "btn clicked  $text", Toast.LENGTH_SHORT).show()
                 val intent = Intent(this@MainActivity, HelloWorldActivity::class.java)
+                intent.putExtra("control", "DADU")
+                intent.putExtra("age", 56)
+                intent.putExtra("username", text)
                 startActivity(intent)
             }
 
