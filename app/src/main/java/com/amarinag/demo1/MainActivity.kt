b47a235d82dbc7457f11b1d6b50e30163e3eed8e
AMarinaG <alberto.marina.gonzalo@gmail.com>
2022-03-30 10:46:55 +0200

    added edittext on FirstActivity

--- app/src/main/java/com/amarinag/demo1/MainActivity.kt
+++ app/src/main/java/com/amarinag/demo1/MainActivity.kt
@@ -3,6 +3,7 @@ package com.amarinag.demo1
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
@@ -14,16 +15,25 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val btnNext: Button = findViewById(R.id.btn_next)
-        val info: TextView = findViewById(R.id.tv_info)
-        info.text = getString(R.string.username)
 
+        btnNext.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(p0: View?) {
+                Log.d("AMG", "Buton pulsado")
+                Toast.makeText(this@MainActivity, "btn clicked", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this@MainActivity, HelloWorldActivity::class.java)
+                startActivity(intent)
+            }
+
+        })
+        /*
         btnNext.setOnClickListener {
             Log.d("AMG", "Buton pulsado")
             Toast.makeText(this@MainActivity, "btn clicked", Toast.LENGTH_SHORT).show()
 
             val intent = Intent(this, HelloWorldActivity::class.java)
             startActivity(intent)
-        }
+        })
+        */
 
     }
 }
